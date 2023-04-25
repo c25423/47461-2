4cbddd1fe0b0cf4c5deaedfc025648040a579b54
c25423 <c25423@gmail.com>
2023-04-25 20:48:10 +0800

    fix: del length limit in ab2

--- models/generated/airbyte_ctes/halo/local_1024_ab2.sql
+++ models/generated/airbyte_ctes/halo/local_1024_ab2.sql
@@ -6,7 +6,7 @@
 -- SQL model to cast each column to its adequate SQL type converted from the JSON schema type
 -- depends_on: {{ ref('local_1024_ab1') }}
 select
-    cast({{ adapter.quote('string') }} as {{ dbt_utils.type_string() }}(1024)) as {{ adapter.quote('string') }},
+    cast({{ adapter.quote('string') }} as {{ dbt_utils.type_string() }}) as {{ adapter.quote('string') }},
     cast({{ adapter.quote('index') }} as {{ dbt_utils.type_bigint() }}) as {{ adapter.quote('index') }},
     _airbyte_ab_id,
     _airbyte_emitted_at,
